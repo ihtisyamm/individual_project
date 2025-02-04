e89d3f143c73ec226bfe0ba44b69b5f32e472a84
ihtisyamm <tisyam273@gmail.com>
2025-02-04 16:32:20 +0000

    Add test file. Ejection implementation ongoing

--- NoC.py
+++ NoC.py
@@ -30,8 +30,8 @@ class Packet:
 class Flit:
     def __init__(self, id, pid, type, src, dest, time):
         self.id = id
-        self.pid = pid # pid stands for packet id btw
-        self.type = type # head | body | tail
+        self.pid = pid      # pid stands for packet id btw
+        self.type = type    # head | body | tail
         self.src = src
         self.dest = dest
         self.timestamp = time
@@ -40,25 +40,44 @@ class Node:
     def __init__(self, env, id):
         self.env = env
         self.id = id
-        self.single = simpy.Store(env, capacity=1) # for single flit buffer
-        self.shared = simpy.Store(env, capacity=100) # extension buffer in a node
+        self.single = simpy.Store(env, capacity=1)      # for single flit buffer
+        self.shared = simpy.Store(env, capacity=100)    # extension buffer in a node
+        self.queue = simpy.Store(env)                   # ejection queue after packet is ejected
+        self.link = simpy.Resource(env, capacity=2)     # ejection link limit
+        
+        self.env.process(self.process())
 
     def injection(self, packet):
-        pass
+        flits = packet.flitation()
+        for flit in flits:
+            yield self.env.process(self.send(flit))
 
-    def ejection(self):
-        pass
+    def ejection(self, packet):
+        with self.link.request() as req:
+            yield req
+            yield self.queue.put(flit)
+            yield self.env.timeout(1)
 
-    def send(self):
-        yield 
+            if flit.type == "TAIL":
+                print(f"Packet {flit.pid} ejected at time {self.env.now}")
 
+    # forward to another node
+    def send(self, flit):
+        yield self.single.put(flit)
+
+    # determine where if the packet has arrived at the node destination
     def process(self):
+        if node:
+            yield self.env.process(self.ejection(packet))
+        else:
+            yield self.send(flit)
+
+    # assigned a pre-determined path to each flit when it is injected
+    def routing(self, flit):
         pass
 
-    def routing(self):
-        pass
-    
-def create_packet(env, node):
+# generate arbitrary packet size
+def create(env, node):
     pid = 0 # again, packet id
 
     while True:
